c1061e958a8faf84f5622b532396002034e2549b
Sahil Maheshwari <sahilm1711@gmail.com>
2021-09-21 10:36:12 +0530

    Creating the backend and creating frontend folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import { Container,AppBar,Typography,Grow,Grid } from "@material-ui/core"
+const App = () => {
+    return (
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+              <Typography variant="h2" align="center">
+                  Memories
+              </Typography>
+              <img src={memories} alt="memories"/>
+            </AppBar>
+        </Container>
+    )
 }
 
-export default App;
+export default App
